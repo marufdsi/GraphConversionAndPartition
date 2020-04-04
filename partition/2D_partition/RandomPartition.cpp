dcc31f3061991b9a02a0a1fe05d48adc54b81095
Maruf Hossain <marufhossain.dsi@gmail.com>
2020-04-04 18:28:40 -0400

    Project updated

--- partition/2D_partition/RandomPartition.cpp
+++ partition/2D_partition/RandomPartition.cpp
@@ -51,20 +51,11 @@ int RandomPartition::partition(int *i_idx, int *j_idx, ValueType *values, int n,
         }
         idx_t condition = (num_row * (_part/col + 1)) > n ? n : (num_row * (_part/col + 1));
         if((i+1) >= condition) {
-//            int startIdx = _part*num_row;
-//            printf("Part=%d, row=%d, col=%d\n", _part, row, col);
             for (int cl = 0; cl < col; ++cl) {
-//                FILE *newMat;
                 std::ofstream outfile;
                 char mat_filename[MAXLINE];
                 sprintf(mat_filename, "%s_random_%d_%d", outdir.c_str(), nparts, (_part+cl));
                 outfile.open(mat_filename, std::ios::out | std::ios::trunc );
-                /*if (!(newMat = fopen(std::strcat(mat_filename, ".mtx"), "w"))) {
-                    std::cerr << "fopen: failed to open file '" << mat_filename << "'" << std::endl;
-                    return 1;
-                }*/
-//                fprintf(newMat, "%%%MatrixMarket matrix coordinate real general\n");
-//                fprintf(newMat, "%d %d %d\n", nVartex, n, nEdges_part[_part + cl]);
                 outfile << "%%%MatrixMarket matrix coordinate real general" << std::endl;
                 outfile << nVartex << " "<< n << " " << nEdges_part[_part + cl] << std::endl;
 
@@ -75,19 +66,12 @@ int RandomPartition::partition(int *i_idx, int *j_idx, ValueType *values, int n,
                     }
                     for (v = i_idx[u]; v < i_idx[u + 1]; v++) {
                         if(new_ids[j_idx[v]]/num_row == cl) {
-//                            fprintf(newMat, "%d %d %lf\n", (new_ids[u] + 1), (new_ids[j_idx[v]] + 1), (double) values[v]);
                             outfile << (new_ids[u] + 1) << " "<< (new_ids[j_idx[v]] + 1) << " " << values[v] << std::endl;
                         }
                     }
                 }
-//                printf("Done=%d\n", cl);
-
 //                 close file
                 outfile.close();
-                /*if (fclose(newMat) != 0) {
-                    std::cerr << "fopen: failed to close file '"<<  mat_filename << "'" <<std::endl;
-                    return 1;
-                }*/
             }
             nVartex = 0;
             start = i + 1;
