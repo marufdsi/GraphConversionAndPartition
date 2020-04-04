a5e92ddecf4c8aa3fbc767661ae0ce16d964afe2
Maruf Hossain <marufhossain.dsi@gmail.com>
2020-04-04 17:49:54 -0400

    Project updated

--- partition/2D_partition/RandomPartition.cpp
+++ partition/2D_partition/RandomPartition.cpp
@@ -76,10 +76,11 @@ int RandomPartition::partition(int *i_idx, int *j_idx, ValueType *values, int n,
 //                printf("Done=%d\n", cl);
 
 //                 close file
-                if (fclose(newMat) != 0) {
-                    std::cerr << "fopen: failed to open file '"<<  mat_filename << "'" <<std::endl;
+                newMat.close();
+                /*if (fclose(newMat) != 0) {
+                    std::cerr << "fopen: failed to close file '"<<  mat_filename << "'" <<std::endl;
                     return 1;
-                }
+                }*/
             }
             nVartex = 0;
             start = i + 1;
