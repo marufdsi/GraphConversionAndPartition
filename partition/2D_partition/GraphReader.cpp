e6a70d7b09526d02f57d17d81a6ae10ed14eef94
Maruf <maruf.csea1@gmail.com>
2020-04-08 01:30:33 -0400

    Log file added

--- partition/2D_partition/GraphReader.cpp
+++ partition/2D_partition/GraphReader.cpp
@@ -10,7 +10,7 @@
 #include <cstring>
 
 idx_t GraphReader::readMatrix(const std::string filename, idx_t *n, idx_t *nnz, idx_t **row_ptr, idx_t **col_ptr, ValueType **val_ptr){
-    idx_t nrows, ncols, nz_elements;
+    idx_t nrows, ncols, nz_elements, isSymmetric=0;
     char banner[MM_MAX_TOKEN_LENGTH];
     char mtx[MM_MAX_TOKEN_LENGTH];
     char crd[MM_MAX_TOKEN_LENGTH];
@@ -21,6 +21,8 @@ idx_t GraphReader::readMatrix(const std::string filename, idx_t *n, idx_t *nnz, 
     f_head >> banner >> mtx >> crd >> data_type >> storage_scheme;
     std::cout<< banner << "\t" << mtx << "\t" << crd << "\t" << data_type << "\t" << storage_scheme << std::endl;
     f_head.close();
+    if (strcmp(storage_scheme, MM_SYMM_STR) == 0)
+        isSymmetric = 1;
     if (std::strcmp(storage_scheme, MM_SYMM_STR) != 0 && std::strcmp(storage_scheme, MM_GENERAL_STR) != 0){
         std::cout<<"Matrix is not symmetric or general!" <<std::endl;
         return MM_UNSUPPORTED_TYPE;
@@ -31,20 +33,10 @@ idx_t GraphReader::readMatrix(const std::string filename, idx_t *n, idx_t *nnz, 
 
 // Read defining parameters:
     fin >> nrows >> ncols >> nz_elements;
-    *n = nrows;
-    *nnz = nz_elements;
-    std::cout<<"rows: " << nrows << " nnz elements: " << nz_elements << std::endl;
-    /// Initialize CSR row, col and value pointer.
-    (*row_ptr) = (idx_t *) calloc((nrows + 1), sizeof(idx_t));
-    (*col_ptr) = (idx_t *) malloc(nz_elements * sizeof(idx_t));
-    (*val_ptr) = (ValueType *) malloc(nz_elements * sizeof(ValueType));
-    (*row_ptr)[0] = 0;
-    for (int i = 0; i < nrows; i++) {
-        if((*row_ptr)[i] != 0){
-            std::cout<<"Not zer for " << (*row_ptr)[i] << " index: " << i << std::endl;
-        }
+    idx_t *csrRowPtrA_counter = (idx_t *) malloc((nrows + 1) * sizeof(idx_t));
+    for (i = 0; i < nrows + 1; ++i) {
+        csrRowPtrA_counter[i] = 0;
     }
-
     /* reserve memory for vector */
     idx_t *i_idx = (idx_t *) malloc(nz_elements * sizeof(idx_t));
     idx_t *j_idx = (idx_t *) malloc(nz_elements * sizeof(idx_t));
@@ -69,39 +61,66 @@ idx_t GraphReader::readMatrix(const std::string filename, idx_t *n, idx_t *nnz, 
 
         idxi--;
         idxj--;
-        ((*row_ptr)[idxi])++;
+        csrRowPtrA_counter[idxi]++;
         i_idx[i] = idxi;
         j_idx[i] = idxj;
         values[i] = fval;
     }
-    for (int i = 0, cumsum = 0; i < nrows; i++) {
-        int temp = (*row_ptr)[i];
-        (*row_ptr)[i] = cumsum;
-        cumsum += temp;
-    }
-    (*row_ptr)[nrows] = nz_elements;
-
-    for (int n = 0; n < nz_elements; n++) {
-        int row = i_idx[n];
-        if (row < 0 || row >= nrows) {
-            printf("out of bound for row=%d\n", row);
-        }
-        int dest = (*row_ptr)[row];
-        (*col_ptr)[dest] = j_idx[n];
-        (*val_ptr)[dest] = values[n];
-
-        (*row_ptr)[row]++;
-    }
-
-    for (int i = 0, last = 0; i <= nrows; i++) {
-        int temp = (*row_ptr)[i];
-        (*row_ptr)[i] = last;
-        last = temp;
-    }
-
     fin.close();
+    if (isSymmetric) {
+        for (i = 0; i < nz; i++) {
+            if (i_idx[i] != j_idx[i])
+                csrRowPtrA_counter[j_idx[i]]++;
+        }
+    }
+    idx_t old_val, new_val;
+    old_val = csrRowPtrA_counter[0];
+    csrRowPtrA_counter[0] = 0;
+    for (i = 1; i <= nrows; i++) {
+        new_val = csrRowPtrA_counter[i];
+        csrRowPtrA_counter[i] = old_val + csrRowPtrA_counter[i - 1];
+        old_val = new_val;
+    }
+    /// Initialize CSR row, col and value pointer.
+    *n = nrows;
+    *nnz = csrRowPtrA_counter[nrows];
+    std::cout<<"rows: " << nrows << " nnz elements: " << *nnz << std::endl;
+    (*row_ptr) = (idx_t *) malloc((nrows + 1) * sizeof(idx_t));
+    memcpy((*row_ptr), csrRowPtrA_counter, (nrows + 1) * sizeof(idx_t));
+    memset(csrRowPtrA_counter, 0, (nrows + 1) * sizeof(idx_t));
+    (*col_ptr) = (idx_t *) malloc((*nnz) * sizeof(idx_t));
+    (*val_ptr) = (ValueType *) malloc((*nnz) * sizeof(ValueType));
+
+    if (isSymmetric) {
+        for (i = 0; i < nz_elements; i++) {
+            if (i_idx[i] != j_idx[i]) {
+                int offset = (*row_ptr)[i_idx[i]] + csrRowPtrA_counter[i_idx[i]];
+                (*col_ptr)[offset] = j_idx[i];
+                (*val_ptr)[offset] = values[i];
+                csrRowPtrA_counter[i_idx[i]]++;
+
+                offset = (*row_ptr)[j_idx[i]] + csrRowPtrA_counter[j_idx[i]];
+                (*col_ptr)[offset] = j_idx[i];
+                (*val_ptr)[offset] = values[i];
+                csrRowPtrA_counter[j_idx[i]]++;
+            } else {
+                int offset = (*row_ptr)[csrRowIdxA_tmp[i]] + csrRowPtrA_counter[csrRowIdxA_tmp[i]];
+                (*col_ptr)[offset] = j_idx[i];
+                (*val_ptr)[offset] = values[i];
+                csrRowPtrA_counter[i_idx[i]]++;
+            }
+        }
+    } else {
+        for (i = 0; i < nz_elements; i++) {
+            int offset = (*row_ptr)[i_idx[i]] + csrRowPtrA_counter[i_idx[i]];
+            (*col_ptr)[offset] = j_idx[i];
+            (*val_ptr)[offset] = values[i];
+            csrRowPtrA_counter[i_idx[i]]++;
+        }
+    }
     free(i_idx);
     free(j_idx);
     free(values);
+    free(csrRowPtrA_counter);
     return 0;
 }
